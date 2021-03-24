3f20c640e8d3a7f4c150d6947110d7035e59942c
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2021-03-24 14:47:54 +0200

    Upload project

---
+++ Lab01/Lab01/Article.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Article
+    {
+        private int article_id;
+        private String title;
+        private String content;
+        private DateTime published_at;
+        private DateTime updated_at;
+        private int views;
+
+        public Article(int Id)
+        {
+            this.article_id = Id;
+            this.published_at = new DateTime();
+            this.updated_at = new DateTime();
+            this.views = 0;
+        }
+
+        public DateTime UpdatedAt
+        {
+            get
+            {
+                return this.updated_at;
+            }
+            set
+            {
+                this.updated_at = value;
+            }
+        }
+
+        public String Title
+        {
+            get
+            {
+                return this.title;
+            }
+            set
+            {
+                // set the title
+                if (value.Length > 10 && value.Length < 96)
+                {
+                    this.title = value;
+                }
+            }
+        }
+
+        public String Content
+        {
+            get
+            {
+                return this.content;
+            }
+            set
+            {
+                if (value.Length > 36 && value.Length < 600)
+                {
+                    this.content = value;
+                }
+            }
+        }
+
+        public override String ToString()
+        {
+            return "Updated at: " + UpdatedAt.ToString() + "\n\n" +
+                Title + "\n\n\n" + Content + "\n\n" + Views;
+        }
+
+        public int Views
+        {
+            get
+            {
+                return this.views;
+            }
+        }
+
+        public void IncreaseViews()
+        {
+            this.views += 1;
+        }
+    }
+
+    
+}
---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is the Lab01 project for Singleton and Factory Method patterns");
+
+            // TODO: Create ArticleService singleton instance here
+        }
+
+        // Declare additional methods and classes you may need
+    }
+}
